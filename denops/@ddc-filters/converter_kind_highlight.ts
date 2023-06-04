cc9722c98db4ad786ebe020bb359fb6123f6a076
natsu <natsu24240722@gmail.com>
2023-06-05 03:21:10 +0900

    create converter.

---
+++ denops/@ddc-filters/converter_kind_highlight.ts
@@ -0,0 +1,35 @@
+import {
+  BaseFilter,
+  Item,
+} from "https://deno.land/x/ddc_vim@v3.5.0/types.ts";
+
+type Params = {
+  kindHighlights: Record<string, string>;
+};
+
+export class Filter extends BaseFilter<Params> {
+  override filter(args: {
+    filterParams: Params,
+    completeStr: string,
+    items: Item[],
+  }): Promise<Item[]> {
+    for (const item of args.items) {
+      if (item.kind) {
+        item.highlights = [{
+          name: "ddc-filter-converter_kind_highlight",
+          type: "kind",
+          hl_group: args.filterParams.kindHighlights[item.kind] ?? "",
+          col: 0,
+          width: item.kind.length,
+        }]
+      }
+    }
+    return Promise.resolve(args.items);
+  }
+
+  override params(): Params {
+    return {
+      kindHighlights: {}
+    };
+  }
+}
